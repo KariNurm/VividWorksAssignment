331e450c0c54bffad2714cb2a0f3c83d7c46e9aa
KariNurm <karinurm@gmail.com>
2023-08-15 17:00:51 +0300

    Added initial typing logic for shelfs.

--- LightResolverLogic/LightResolverModule.cs
+++ LightResolverLogic/LightResolverModule.cs
@@ -19,8 +19,69 @@ namespace LightResolver.Logic
         /// </summary>
         /// <param name="composition"></param>
         /// <returns></returns>
+        /// 
+
         public OptimizationResult OptimizeComposition(Composition composition)
         {
+            int Wattage = 0;
+            int Price = 0;
+                 
+
+
+            for (int i = 0; i < composition.Sections.Count; i++)
+            {
+
+                Section section = composition.Sections[i];
+                
+
+                for(int j = 0; j < section.Shelves.Count; j++)
+                {
+                    Shelf shelf = section.Shelves[j];
+                    Shelf previousShelf = composition.ShelfAt(i - 1, j);
+
+                    int lastSectionIndex = composition.Sections.Count - 1;
+
+                    bool hasLight = shelf.HasLight;
+
+                    //power left only
+
+                    if (i == 0) // First section
+                    {
+                        shelf.Type = hasLight ? ShelfType.G1 : ShelfType.E;
+                    }
+                    else if (i == lastSectionIndex) // Last section
+                    {
+                        shelf.Type = hasLight ? ShelfType.H : ShelfType.E;
+                        if (previousShelf.Type == ShelfType.E) previousShelf.Type = ShelfType.EI;
+                    }
+                    else if(i != 0) // Middle section
+                    {
+                        if(hasLight)
+                        {
+                            shelf.Type = ShelfType.H;
+                            if(previousShelf.HasLight && i - 1 != 0)
+                            {
+                                previousShelf.Type = ShelfType.H;
+                            }
+                            else if(previousShelf.HasLight && i - 1 == 0)
+                            {
+                                previousShelf.Type = ShelfType.G1;
+                            }
+                            else if (!previousShelf.HasLight && i - 1 == 0)
+                            {
+                                previousShelf.Type = ShelfType.F1;
+                            }
+                        }
+                        else
+                        {
+                            shelf.Type = ShelfType.E;
+                        }
+                    }
+                    
+                }
+
+            }
+            
             //TODO: Create logic that calculates the optimal lighting composition with the cheapest price
 
             //TODO: Update the composition model objects with OuterWallTypes and ShelfTypes
